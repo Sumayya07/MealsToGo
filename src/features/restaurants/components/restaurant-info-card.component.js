daba4ec6b7e373d4457d37ff0a2ce55b9c3f5a3c
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-02-27 20:42:03 +0530

    Added the styled-components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components/native"
-import { Text, StyleSheet } from "react-native";
+import { Text } from "react-native";
 import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+    background-color: white
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+     background-color: white
+     padding: 20px
+`;
 
 const Title = styled.Text`
     padding: 16px
     color: red
 `;
-
-const 
  
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -25,20 +32,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     } = restaurant
 
     return (
-        <Card elevation={5} style={styles.card}>
-        <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0]}}/>
+        <RestaurantCard elevation={5} >
+        <RestaurantCardCover key={name} source={{ uri: photos[0]}}/>
         <Title>{name}</Title>
-        </Card>   
+        </RestaurantCard>   
     )
     
 }
-
-const styles = StyleSheet.create({
-    card: {
-        backgroundColor: "white"
-    },
-    cover: {
-        backgroundColor: "white",
-        padding: 20
-    }
-})
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,32 +1,34 @@
 import React from "react";
+import styled from "styled-components/native"
 import { Searchbar } from "react-native-paper";
 import {View, Text, SafeAreaView, StyleSheet, StatusBar} from "react-native";
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component"
+
+
+const SafeArea = styled(SafeAreaView)`
+    flex: 1;
+    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+   padding: 16px;
+   background-color: white;
+`;
+
+const RestaurantListContainer = styled.View`
+    flex: 1;
+    padding: 16px;
+    background-color: white;
+`;
 
 
 export const RestaurantsScreen = () => (
-    <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
+    <SafeArea>
+    <SearchContainer>
         <Searchbar />
-    </View>
-    <View style={styles.list}>
+    </SearchContainer>
+    <RestaurantListContainer>
       <RestaurantInfoCard />
-    </View>
-    </SafeAreaView>
+    </RestaurantListContainer>
+    </SafeArea>
 )
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1, 
-      marginTop: StatusBar.currentHeight
-    },
-    search: {
-      padding: 16, 
-      backgroundColor:"white"
-    }, 
-    list: {
-      padding: 16,
-      backgroundColor: "blue",
-      flex: 1
-    }
-  });
