e00c9f42d9d3cff4f2a9064abe44750cd87a35f6
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-03-02 13:02:10 +0530

    Used SvgXml with Rating

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components/native"
 import { Text } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg"
+import star from '../../../../assets/star'
 
 const RestaurantCard = styled(Card)`
     background-color: ${(props) => props.theme.colors.bg.primary}
@@ -12,12 +14,27 @@ const RestaurantCardCover = styled(Card.Cover)`
      padding: ${(props) => props.theme.space[3]}
 `;
 
-const Title = styled.Text`
-    font-family: ${(props) => props.theme.fonts.body}
+const Info = styled.View`
     padding: ${(props) => props.theme.space[3]}
+`
+
+const Title = styled.Text`
+    font-family: ${(props) => props.theme.fonts.heading}
+    font-size: ${(props) => props.theme.fontSizes.body}
     color: ${(props) => props.theme.colors.ui.primary}
 `;
- 
+
+const Address = styled.Text`
+    font-family: ${(props) => props.theme.fonts.body}
+    font-size: ${(props) => props.theme.fontSizes.caption}
+`
+
+const Rating = styled.View`
+    flex-direction: row;
+    padding-top: ${(props) => props.theme.space[2]}
+    padding-bottom: ${(props) => props.theme.space[2]}
+`
+
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
     const {
@@ -30,12 +47,22 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
      isOpenNow = true,
      rating = 4,
      isClosedTemporarily,
-    } = restaurant
+    } = restaurant;
+
+    const ratingArray = Array.from(new Array(Math.floor(rating)))
 
     return (
         <RestaurantCard elevation={5} >
-        <RestaurantCardCover key={name} source={{ uri: photos[0]}}/>
-        <Title>{name}</Title>
+         <RestaurantCardCover key={name} source={{ uri: photos[0]}}/>
+        <Info>
+         <Title>{name}</Title>
+         <Rating>
+         {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+         ))}
+         </Rating>
+         <Address>{address}</Address>
+        </Info>
         </RestaurantCard>   
     )
     
