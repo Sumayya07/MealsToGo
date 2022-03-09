5e74dbfbe0d1c33d929d6f56b4a7a588e3d209a6
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-03-09 19:41:59 +0530

    React Native Maps

---
+++ src/features/map/screens/map.screen.js
@@ -0,0 +1,8 @@
+import React, { useState, useContext, useEffect } from 'react';
+import MapView from 'react-native-maps'
+
+export const MapScreen = () => {
+    return (
+        <MapView />
+    )
+}
