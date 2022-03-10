32a6ea2c1e6055581e3296fe1775f77c2e9e5157
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-03-10 16:57:37 +0530

    Hooking Up Favourites

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import { theme } from './src/infrastructure/theme'
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context"
 import { LocationContextProvider } from './src/services/location/location.context';
+import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 import { Navigation } from './src/infrastructure/navigation';
 
 export default function App() {
@@ -25,11 +26,13 @@ export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
-    <LocationContextProvider>
-      <RestaurantsContextProvider>
+     <FavouritesContextProvider>
+      <LocationContextProvider>
+       <RestaurantsContextProvider>
         <Navigation />
-      </RestaurantsContextProvider>
+       </RestaurantsContextProvider>
       </LocationContextProvider>
+     </FavouritesContextProvider>
     </ThemeProvider>
     <ExpoStatusBar style='auto' /> 
     </>
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SvgXml } from "react-native-svg"
 import { Spacer } from "../../../components/spacer/spacer.component"
 import { Text } from "../../../components/typography/text.component"
+import { Favourite } from "../../../components/favourites/favourite.component";
 
 import star from '../../../../assets/star'
 import open from '../../../../assets/open'
@@ -37,6 +38,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
     return (
         <RestaurantCard elevation={5} >
+         <Favourite />
          <RestaurantCardCover key={name} source={{ uri: photos[0]}}/>
         <Info>
          <Text variant="label">{name}</Text>
