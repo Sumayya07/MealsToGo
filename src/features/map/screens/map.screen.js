a0ff4e6b78003ae54a248c2bd7c4a77290806239
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-03-10 11:16:53 +0530

    Setting up the Map Region

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -2,6 +2,9 @@ import React, { useState, useContext, useEffect } from 'react';
 import MapView from 'react-native-maps'
 import styled from 'styled-components/native'
 import { Search } from '../components/search.component'
+import { LocationContext } from '../../../services/location/location.context';
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context'
+
 
 const Map = styled(MapView)`
     height: 100%
@@ -9,10 +12,39 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+    const { location } = useContext(LocationContext)
+    const { restaurants = [] } = useContext(RestaurantsContext)
+
+    const [latDelta, setLatDelta] = useState(0)
+
+    const { lat, lng, viewport } = location;
+
+    useEffect(() => {
+        const northeastLat = viewport.northeast.lat;
+        const southwestLat = viewport.southwest.lat
+
+        const latDelta = northeastLat - southwestLat
+        setLatDelta(latDelta)
+
+    }, [location, viewport])
+
     return (
         <>
         <Search />
-        <Map />
+        <Map
+        region={{
+            latitude: lat,
+            longitude: lng,
+            latitudeDelta: latDelta,
+            longitudeDelta: 0.02
+        }}
+        
+        >
+        {restaurants.map((restaurant) => {
+            return null;
+        })}
+
+        </Map>
         </>
         
     )
