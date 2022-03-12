9cc654d5b78237c5e9753f5d79e60a86b52b24c8
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-03-12 15:17:52 +0530

    Authentication and service context

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from "react";
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
@@ -8,8 +8,25 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 import { Navigation } from './src/infrastructure/navigation';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsksXAsuXLgeS5Tnd52gSvk4M2qfZqPQE",
+  authDomain: "fooddeliveryapp-27826.firebaseapp.com",
+  projectId: "fooddeliveryapp-27826",
+  storageBucket: "fooddeliveryapp-27826.appspot.com",
+  messagingSenderId: "502881454876",
+  appId: "1:502881454876:web:2ff2f7a9fecb5784d9c8d9"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { View, Text } from 'react-native'
 import { AppNavigator } from './app.navigator'
+import { AuthenticationContext } from '../../services/authentication/authentication.context'
 
 export const Navigation = () => {
-    return <AppNavigator />
+    const { isAuthenticated } = useContext(AuthenticationContext)
+    return isAuthenticated ? (
+    <AppNavigator /> 
+    ) : (
+        <View>
+            <Text>Not Authenticated</Text>
+        </View>
+    )
 }
